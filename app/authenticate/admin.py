5ea347dc7b4931d85eea4f6a3d94efc65e49d227
DeatheDis <disdos93@gmail.com>
2025-09-23 13:01:55 +0300

    added postgresql and swagger

---
+++ app/authenticate/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'is_active', 'is_superuser')
+    list_display_links = ('id', 'email')
+    search_fields = ('email',)
+    list_filter = ('email',)
